18089ef92ed89c273ccd27b924dadd30d1ca6294
luarmorv4 <lesbestcompte@gmail.com>
2024-12-31 20:05:20 -0400

    Create 3b2169cf53bc6104dabe8e19562e5c2.lua

---
+++ 3b2169cf53bc6104dabe8e19562e5c2.lua
@@ -0,0 +1,7 @@
+getgenv().Team = "Pirates"
+
+getgenv().FixCrash = false -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
+
+getgenv().FixCrash2 = false -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/luarmorv4/api.luarmor.net-files-v3-loaders-/refs/heads/main/3b2169cf53bc6104dabe8e19562e5cc2.lua',true))()
